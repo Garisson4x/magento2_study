dfbb054d948360257a78c3b827472dfba6b26262
Garisson <strelnikov.prog@gmail.com>
2019-09-20 17:25:02 +0300

    NewControllers update

--- app/code/Shellpea/HelloWorld/MyView.php
+++ app/code/Shellpea/HelloWorld/MyView.php
@@ -3,8 +3,21 @@ namespace Shellpea\HelloWorld;
 use Magento\Catalog\Controller\Product\View;
 class MyView extends View
 {
+    protected $_rawResult;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\ForwardFactory $resultForwardFactory,
+        \Magento\Catalog\Helper\Product\View $viewHelper,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\Controller\Result\Raw $rawResult)
+    {
+        parent::__construct($context, $viewHelper, $resultForwardFactory, $resultPageFactory);
+        $this->_rawResult = $rawResult;
+    }
+
     public function execute()
     {
-        die('123');
+        return $this->_rawResult->setContents('There are no products, sorry');
     }
 }
