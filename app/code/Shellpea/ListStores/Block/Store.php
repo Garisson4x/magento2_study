5687bd7e0347ab33716dd235a0b3abb76af54c81
Garisson <strelnikov.prog@gmail.com>
2019-10-04 16:57:24 +0300

    minor fixed

--- app/code/Shellpea/ListStores/Block/Store.php
+++ app/code/Shellpea/ListStores/Block/Store.php
@@ -2,34 +2,30 @@
 
 namespace Shellpea\ListStores\Block;
 
-use \Magento\Framework\App\ObjectManager;
-
 class Store extends \Magento\Framework\View\Element\Template
 {
-    protected $_storeManager;
+    protected $storeManager;
+
+    protected $categoryFactory;
 
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Catalog\Model\CategoryFactory $categoryFactory,
         array $data = []
-    )
-    {
-        $this->_storeManager = $storeManager;
+    ) {
+        $this->storeManager = $storeManager;
+        $this->categoryFactory = $categoryFactory;
         parent::__construct($context, $data);
     }
 
     public function getStoresName()
     {
         $result = [];
-        $_objectManager = ObjectManager::getInstance();
-
-        foreach ($this->_storeManager->getStores() as $store) {
-            $category = $_objectManager->create('Magento\Catalog\Model\Category')
-                ->load($store->getRootCategoryId());
+        foreach ($this->storeManager->getStores() as $store) {
+            $category = $this->categoryFactory->create()->load($store->getRootCategoryId());
             $result[] = $store->getName() . ' Root Category -> ' . $category->getName();
         }
         return $result;
     }
-
 }
-?>
